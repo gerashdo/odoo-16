51372678f7bf5f60fa8f1c8d7c750deda715f2d3
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-05-26 13:53:23 -0600

    Add folders for models

---
+++ custom/addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    'name':'Real State',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'application': True,
+    'installable': True,
+}
