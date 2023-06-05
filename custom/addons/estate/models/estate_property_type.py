3c8c80a2f8ffe174920eb2b1d7e52972b46906ab
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-05 10:29:35 -0600

    Add properties list in the prop type form

--- custom/addons/estate/models/estate_property_type.py
+++ custom/addons/estate/models/estate_property_type.py
@@ -5,4 +5,5 @@ class PropertyType(models.Model):
     _description = 'Diffrent types for the properties'
 
     name = fields.Char('Nombre', required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id", readonly=True)
     
