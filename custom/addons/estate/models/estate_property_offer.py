07f031f262b575aa0d1a46f29e7d59c5a9514008
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-08 17:06:54 -0600

    Add state button for offers by property type

--- custom/addons/estate/models/estate_property_offer.py
+++ custom/addons/estate/models/estate_property_offer.py
@@ -18,6 +18,7 @@ class PropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", required=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one(related="property_id.property_type_id", store=True)
 
     _sql_constraints = [
         ('check_positive_price','CHECK(price > 0)','El precio debe ser un número positivo'),
--- custom/addons/estate/models/estate_property_type.py
+++ custom/addons/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PropertyType(models.Model):
     _name = 'estate.property.type'
@@ -8,4 +8,11 @@ class PropertyType(models.Model):
     name = fields.Char('Nombre', required=True)
     property_ids = fields.One2many("estate.property", "property_type_id", readonly=True)
     sequence = fields.Integer('Sequence', default=1)
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count", string="Cantidad de ofertas")
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
     
