64d75529270b79afd1fe1ed86a469e56f6ee4bf0
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-06-09 13:15:47 -0600

    Add inheritance to show property in users

--- custom/addons/estate/__manifest__.py
+++ custom/addons/estate/__manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/res_users_views.xml',
     ],
     'application': True,
     'installable': True,
